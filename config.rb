629b73ea00c4d253e9c8bd577ec6f73340184cd7
Robert Bradford <robert.bradford@ldschurch.org>
2015-07-29 16:17:27 -0600

    Add basic I18n and dynamic page setup.

--- config.rb
+++ config.rb
@@ -1,3 +1,5 @@
+activate :i18n, mount_at_root: false
+activate :directory_indexes
 ###
 # Compass
 ###
@@ -27,6 +29,9 @@
 # Proxy pages (https://middlemanapp.com/advanced/dynamic_pages/)
 # proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
 #  :which_fake_page => "Rendering a fake page with a local variable" }
+["tom", "dick", "harry"].each do |name|
+  proxy "/about/#{name}.html", "/localizable/about/template.html", :locals => { :person_name => name }, :ignore => true
+end
 
 ###
 # Helpers
